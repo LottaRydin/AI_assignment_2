b156c7a3acef9a6651631d933186e2b7dce62884
Lotta Rydin <lotta.rydin@telia.com>
2021-09-15 08:58:00 +0200

    huvud

--- assignment2.R
+++ assignment2.R
@@ -6,4 +6,9 @@ myFunction <- function(move_info, water_stats, human_pos, edges, water_dev){
   # human_pos: A vector giving the positions of the two tourists (elements 1 and 2) and yourself (element 3).
   # edges: two column matrix giving the edges paths between waterholes (edges) present (the numbers are from and to numbers for the waterholes)
   # water_dev: standard deviation of readings for salinity, phosphate and nitrogen respectively at each waterhole
+  
+  
+  
+  
+  
 }
