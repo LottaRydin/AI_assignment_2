7de6a894ae7221f58a1806ddc8e4b9aa23b3f55f
Vendela Andersson <vendela.a97@gmail.com>
2021-09-15 10:00:37 +0200

    first session

--- assignment2.R
+++ assignment2.R
@@ -7,8 +7,28 @@ myFunction <- function(moveInfo, readings, positions, edges, probs){
   # edges: two column matrix giving the edges paths between waterholes (edges) present (the numbers are from and to numbers for the waterholes)
   # probs: standard deviation of readings for salinity, phosphate and nitrogen respectively at each waterhole
   
+  print(readings)
+  sal = readings[1]
+  pho = readings[2]
+  nit = readings[3]
+  
+  matches = list(sal = c(), pho = c(), nit = c())
+  for (i in 1:3) {
+    for (j in 1:nrow(probs[[1]])){
+      if (readings[i] > probs[[i]][j, 1]-probs[[i]][j, 2] & readings[i] < probs[[i]][j, 1]+probs[[i]][j, 2]){
+        matches[[i]] = append(matches[[i]], j)
+      }
+    }
+  }
+  print(probs)
+  print(matches)
+  print(edges)
   
   
-  
+}
+
+path_finder <- function(goal, position, visited){
+  visited = append(visited, position)
+  neighbors <- c()
   
 }
