a274e08b5c6146158ab3c82dd89186a41d1be297
Lotta Rydin <lotta.rydin@telia.com>
2021-09-15 08:56:55 +0200

    huvud

---
+++ assignment2.R
@@ -0,0 +1,9 @@
+library(WheresCroc)
+
+myFunction <- function(move_info, water_stats, human_pos, edges, water_dev){
+  # move_info:  A list of information for the move. This has two fields: moves and mem
+  # water_stats: A vector giving the salinity, phosphate and nitrogen reading from Croc sensors at his current location.
+  # human_pos: A vector giving the positions of the two tourists (elements 1 and 2) and yourself (element 3).
+  # edges: two column matrix giving the edges paths between waterholes (edges) present (the numbers are from and to numbers for the waterholes)
+  # water_dev: standard deviation of readings for salinity, phosphate and nitrogen respectively at each waterhole
+}
