2548b9aa83615798ac444d477ced60ad25d1c07d
Vendela Andersson <vendela.a97@gmail.com>
2021-09-16 12:05:27 +0200

    added transition matrix

--- assignment2.R
+++ assignment2.R
@@ -20,12 +20,79 @@ myFunction <- function(moveInfo, readings, positions, edges, probs){
   print(probs)
   print(matches)
   print(edges)
+  print(path_finder(15, positions[3], c(), edges, c()))
+  print('mem')
+  print(length(moveInfo$mem))
+  
+  if (length(moveInfo$mem) == 1) {
+    trans_mat = matrix(0, 40, 40)
+    for (node in 1:40){
+      neighbor = c()
+      for (i in 1:2){
+        for (j in 1:nrow(edges)){
+          if (edges[j, i] == node) {
+            neighbor = append(neighbor, edges[j,-i])
+          }
+        }
+      }
+      prob = 1/(length(neighbor)+1)
+      for (i in 1:length(neighbor)){
+        trans_mat[node, neighbor[i]] = prob
+      }
+      trans_mat[node, node] = prob
+    }
+    print(trans_mat)
+    moveInfo$mem[[2]] = trans_mat
+  }
+  print('mem')
+  print(moveInfo$mem)
   
   
 }
 
-path_finder <- function(goal, position, visited){
-  visited = append(visited, position)
-  neighbors <- c()
+path_finder <- function(goal, position, visited, edges, path){
+  frontier = list()
+  visited = c()
+  expand = list()
+  first_pos = list(pos=position, path=c())
+  frontier = append(frontier, list(first_pos))
+  while (length(frontier) != 0){
+    expand = frontier[[1]]
+    visited = append(visited, expand$pos)
+    frontier = frontier[-1]
+    if (expand$pos == goal) {
+      return (expand$path)
+    } else {
+      for (i in 1:2){
+        for (j in 1:nrow(edges)){
+          if (edges[j, i] == expand$pos & !(edges[j, -i] %in% visited)){
+            neighbor = list(pos=edges[j,-i], path=append(expand$path, expand$pos))
+            frontier = append(frontier, list(neighbor))
+          }
+        }
+      }
+    }
+  }
   
+  
+  # print(cat('position', position))
+  # path = append(path, position)
+  # neighbors <- c()
+  # visited = append(visited, position)
+  # for (i in 1:2){
+  #   for (j in 1:nrow(edges)){
+  #     if (edges[j, i] == position){
+  #       neighbors = append(neighbors, edges[j,-i])
+  #     }
+  #   }
+  # }
+  # for (i in 1:length(neighbors)){
+  #   if (!(neighbors[i] %in% visited)) {
+  #     if (neighbors[i] == goal){
+  #       return (path)
+  #     } else {
+  #       return (path_finder(goal, neighbors[i], visited, edges, path))
+  #     }
+  #   }
+  # }
 }
